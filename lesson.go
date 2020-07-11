e8cf1c9999a3ed7c71ace3afc80584218d2e5d88
daisuke8000 <daisuke12108000@gmail.com>
2020-07-11 17:59:32 +0900

    golang_lesson クロージャ

--- lesson.go
+++ lesson.go
@@ -297,7 +297,41 @@ func main() {
 	}(1)
 }
 
- */
+----------------------------------
+
+	<<golang_lesson クロージャ>>
+----------------------------------
+
+func incrementGenerator() (func() int) {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func cicleArea(pi float64) func(radius float64) float64{
+	return func(radius float64) float64{
+		return pi * radius * radius
+	}
+}
+
+func main() {
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+
+	c1 := cicleArea(3.14)
+	fmt.Println(c1(2))
+
+	c2 := cicleArea(3)
+	fmt.Println(c2(2))
+
+}
+
+*/
 
 func main() {
 }
