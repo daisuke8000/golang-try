df98ff36491f7ecc15051c7fb308587f6f20dfe2
daisuke8000 <daisuke12108000@gmail.com>
2020-07-18 13:14:53 +0900

    golang_lesson Embedded

--- lesson.go
+++ lesson.go
@@ -757,28 +757,94 @@ func main() {
 	fmt.Println(j2)
 }
 
-*/
+----------------------------------
+<<golang_lesson Embedded>>
+----------------------------------
 
 type Vertex struct{
-	X, Y int
+	x, y int
 }
 
 func (v Vertex) Area() int{
-	return v.X * v.Y
+	return v.x * v.y
 }
 
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
 }
 
-func Area(v Vertex)int{
-	return v.X * v.Y
+type Vertex3D struct{
+	Vertex
+	z int
 }
+
+func (v Vertex3D) Area3D() int{
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y},z}
+}
+
 
 func main() {
-	v := Vertex{3, 4}
+	//v := Vertex{3, 4}
 	//fmt.Println(Area(v))
-	v.Scale(10)
+	v := New(3,4, 5)
+	v.Scale3D(10)
 	fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
+}
+
+*/
+
+type Vertex struct{
+	x, y int
+}
+
+func (v Vertex) Area() int{
+	return v.x * v.y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+type Vertex3D struct{
+	Vertex
+	z int
+}
+
+func (v Vertex3D) Area3D() int{
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y},z}
+}
+
+
+func main() {
+	//v := Vertex{3, 4}
+	//fmt.Println(Area(v))
+	v := New(3,4, 5)
+	v.Scale3D(10)
+	fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
 }
