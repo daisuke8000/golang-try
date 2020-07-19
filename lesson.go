d0b575a211797edf50cef06e03e8b7df90fd74bb
daisuke8000 <daisuke12108000@gmail.com>
2020-07-19 11:41:07 +0900

    golang_lesson goroutine~producer&consumer

--- lesson.go
+++ lesson.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 <<golang_lesson 変数宣言>>
 ----------------------------------
@@ -961,10 +967,144 @@ func main() {
 	fmt.Println(v.plus())
 	fmt.Println(v)
 }
-*/
 
+----------------------------------
+<<golang_lesson goroutine>>
+----------------------------------
+func goroutine(s string, wg *sync.WaitGroup){
+	for i := 0; i < 5; i++{
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+	wg.Done()
+}
+
+func normal(s string){
+	for i := 0; i < 5; i++{
+		//time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("world", &wg)
+	normal("hello")
+	wg.Wait()
+}
+
+----------------------------------
+<<golang_lesson channel>>
+----------------------------------
+func goroutine1(s []int, c chan int){
+	sum := 0
+	for _, v := range s{
+		sum += v
+	}
+	c <- sum
+}
+
+func goroutine2(s []int, c chan int){
+	sum := 0
+	for _, v := range s{
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine1(s, c)
+	go goroutine2(s, c)
+	x := <-c
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
 
 }
 
+
+----------------------------------
+<<golang_lesson range&close>>
+----------------------------------
+func goroutine1(s []int, c chan int){
+	sum := 0
+	for _, v := range s{
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	// c:= make(chan int, len(s))※最初に長さ指定もOK
+	go goroutine1(s, c)
+	for i := range c{
+		fmt.Println(i)
+	}
+}
+----------------------------------
+<<golang_lesson producer&consumer>>
+----------------------------------
+func producer(ch chan int, i int){
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup){
+	for i := range ch{
+		func (){
+			defer wg.Done()
+			fmt.Println("process", i * 1000)
+		}()
+	}
+	fmt.Println("###############")
+}
+
+func main(){
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	//producer
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go producer(ch, i)
+	}
+	go consumer(ch, &wg)
+	wg.Wait()
+	close(ch)
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
+}
+*/
+
+func producer(ch chan int, i int){
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup){
+	for i := range ch{
+		func (){
+			defer wg.Done()
+			fmt.Println("process", i * 1000)
+		}()
+	}
+	fmt.Println("###############")
+}
+
+func main(){
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	//producer
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go producer(ch, i)
+	}
+	go consumer(ch, &wg)
+	wg.Wait()
+	close(ch)
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
+}
