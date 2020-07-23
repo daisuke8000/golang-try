9c23035d8795b095393b7ae8e037bbc3aadd2681
daisuke8000 <daisuke12108000@gmail.com>
2020-07-24 07:25:24 +0900

    database lesson

--- main.go
+++ main.go
@@ -292,4 +292,137 @@ func main() {
 	mva := talib.Ema(spy.Close, 14)
 	fmt.Println(mva)
 }
+type JsonRPC2 struct {
+	Version string `json:"jsonrpc"`
+	Method string `json:"method"`
+	Params interface{} `json:"params"`
+	Result interface{} `json:"result,omitempty"`
+	Id *int `json:"id,omitempty"`
+}
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
+func main() {
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path:"/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(),nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}});
+	err != nil {
+		log.Fatal("subscribe:", err)
+		return
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+		if message.Method == "channelMessage"{
+			log.Println(message.Params)
+		}
+	}
+}
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+var DbConnection *sql.DB
+
+type Person struct {
+	Name string
+	Age int
+}
+
+func main() {
+	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer DbConnection.Close()
+	cmd := `CREATE TABLE IF NOT EXISTS person(
+			name STRING,
+			age INT)`
+	_, err := DbConnection.Exec(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
+	//_, err = DbConnection.Exec(cmd, "Nansy", 20)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//cmd = "UPDATE person SET age = ? WHERE name = ?"
+	//_, err = DbConnection.Exec(cmd, 25, "Mike")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	//cmd = "SELECT * FROM person"
+	//rows, _ := DbConnection.Query(cmd)
+	//defer rows.Close()
+	//var pp []Person
+	//for rows.Next(){
+	//	var p Person
+	//	err := rows.Scan(&p.Name, &p.Age)
+	//	if err != nil{
+	//		log.Println(err)
+	//	}
+	//	pp = append(pp, p)
+	//}
+	//err = rows.Err()
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//for _, p := range pp {
+	//	fmt.Println(p.Name, p.Age)
+	//}
+
+	//cmd = "SELECT * FROM person where age = ?"
+	//row := DbConnection.QueryRow(cmd, 1000)
+	//var p Person
+	//err = row.Scan(&p.Name, &p.Age)
+	//if err != nil {
+	//	if err == sql.ErrNoRows{
+	//		log.Println("No row")
+	//	}else {
+	//		log.Println(err)
+	//	}
+	//}
+	//fmt.Println(p.Name, p.Age)
+
+	//cmd = "DELETE FROM person WHERE name = ?"
+	//_, err = DbConnection.Exec(cmd, "Nancy")
+	//if err != nil{
+	//	log.Fatalln(err)
+	//}
+	tableName := "person; INSERT INTO person (name, age) VALUES('Mr.X',100)"
+	cmd = fmt.Sprintf("SELECT * FROM %s", tableName)
+	rows, _ := DbConnection.Query(cmd)
+	defer rows.Close()
+	var pp []Person
+	for rows.Next(){
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil{
+			log.Println(err)
+		}
+		pp = append(pp, p)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, p := range pp {
+		fmt.Println(p.Name, p.Age)
+	}
+}
 */
+
