228cb0bd14d7418bc80955cfd985ecb7830b8da8
daisuke8000 <daisuke12108000@gmail.com>
2020-07-24 12:27:52 +0900

    webapp-devlop

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
 )
 
 /*
@@ -450,10 +453,70 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main(){
-	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
-	p1.save()
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+	//t, _ := template.ParseFiles(tmpl + ".html")
+	//t.Execute(w, p)
+	// キャッシュを使用する書き方
+	err := templates.ExecuteTemplate(w, tmpl+".html",p)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string){
+	// /view/test
+	p, err := loadPage(title)
+	if err != nil{
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string){
+	// /view/test
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string){
+	// /view/test
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil{
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+
+func main(){
+	//p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	//p1.save()
+	//
+	//p2, _ := loadPage(p1.Title)
+	//fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
